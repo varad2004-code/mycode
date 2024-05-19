f05f286d96d1f9a953fb7b639b6330da45411f2c
varad2004-code <138018860+varad2004-code@users.noreply.github.com>
2024-05-19 15:12:40 +0530

    Add files via upload
    uploaded Basic python code.

---
+++ Basic Python/Loop.py
@@ -0,0 +1,13 @@
+# i=0
+# while i <= 1000000:
+#     print(i)
+#     i+=1
+   
+# i=1
+# while i <= 5:
+#     print(i * "*")
+#     i+=1
+
+#For Loop
+for i in range(5):
+    print(i + 1)
---
+++ Basic Python/operators.py
@@ -0,0 +1,24 @@
+# print(5 + 5)#Addition operator
+# print(5 - 5)#substraction operator
+# print(5 * 5)#multiply
+# print(5 % 5)#modulo
+# print(5 / 5)#divide
+# print(5 // 5)#floor division
+# print(5 ** 5)#power of 5
+
+#Operator Presendence
+# result=( 2 + 3 ) / 5 
+# print(result)
+
+#Comparison Operator
+# print(3 > 2)
+# print(3 < 2)
+# print(3 <= 2)
+# print(3 >= 2)
+# print(3!=3)
+# print(3!=2)
+
+#Logical Operator
+print(2 > 3 or 2 > 1)
+print(3 > 2 and 2 > 1)
+print(not 3 > 2)
